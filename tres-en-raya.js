baa1345550f955270cf4cb6ac23daafac0e282b6
kevwil <108852395+sckayelpro@users.noreply.github.com>
2022-12-29 18:03:57 -0400

    Update tres-en-raya.js

--- tres-en-raya.js
+++ tres-en-raya.js
@@ -4,7 +4,7 @@ const squares = document.querySelectorAll("img");
 const scorePlayer1 = document.querySelector("#score-player-1");
 const scorePlayer2 = document.querySelector("#score-player-2");
 
-const rutasImagenes = ["/images/1.png", "/images/2.png", "/images/3.png","/images/4.png","/images/5.png","/images/6.png","/images/7.png","/images/8.png","/images/9.png"];
+const rutasImagenes = ["1.png", "2.png", "3.png","4.png","5.png","6.png","7.png","8.png","9.png"];
 
 const reset = document.querySelector("#reset-button");
 
